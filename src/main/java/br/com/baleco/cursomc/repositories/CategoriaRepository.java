193b703d48415a633706b75a6ae52b2368b5c76a
Walter Baleco <WBaleco@hotmail.com>
2019-04-07 23:08:30 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/br/com/baleco/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package br.com.baleco.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.baleco.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+
+}
