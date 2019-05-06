30348f4e8c4773ff8f09f811a2d753e45289ce7a
Walter Baleco <WBaleco@hotmail.com>
2019-05-06 20:22:42 -0300

    Cliente, Tipo cliente, Telefones e Endereço

--- src/main/java/br/com/baleco/cursomc/CursomcApplication.java
+++ src/main/java/br/com/baleco/cursomc/CursomcApplication.java
@@ -1,6 +1,5 @@
 package br.com.baleco.cursomc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.baleco.cursomc.domain.Categoria;
 import br.com.baleco.cursomc.domain.Cidade;
+import br.com.baleco.cursomc.domain.Cliente;
+import br.com.baleco.cursomc.domain.Endereco;
 import br.com.baleco.cursomc.domain.Estado;
 import br.com.baleco.cursomc.domain.Produto;
+import br.com.baleco.cursomc.domain.enums.TipoCliente;
 import br.com.baleco.cursomc.repositories.CategoriaRepository;
 import br.com.baleco.cursomc.repositories.CidadeRepository;
+import br.com.baleco.cursomc.repositories.ClienteRepository;
+import br.com.baleco.cursomc.repositories.EnderecoRepository;
 import br.com.baleco.cursomc.repositories.EstadoRepository;
 import br.com.baleco.cursomc.repositories.ProdutoRepository;
 
@@ -31,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -68,6 +78,17 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 	}
 
 }
---
+++ src/main/java/br/com/baleco/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,12 @@
+package br.com.baleco.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.baleco.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+
+}
