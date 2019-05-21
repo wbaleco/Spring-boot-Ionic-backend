6f0c3b0376c62d94495462efede50daa770efb2d
Walter Baleco <WBaleco@hotmail.com>
2019-05-21 00:02:02 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/br/com/baleco/cursomc/domain/Pagamento.java
+++ src/main/java/br/com/baleco/cursomc/domain/Pagamento.java
@@ -10,6 +10,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import br.com.baleco.cursomc.domain.enums.StatusPagamento;
 
 @Entity
@@ -22,7 +24,8 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 
 	private Integer statusPagamento;
-
+	
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
