a275b7a3e8d521b4b18b951c3fdd4128e3b842c7
Walter Baleco <WBaleco@hotmail.com>
2019-08-09 21:21:55 -0300

    Deletando uma categoria com DELETE

--- src/main/java/br/com/baleco/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/baleco/cursomc/resources/CategoriaResource.java
@@ -45,4 +45,12 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 
 	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
 }
--- src/main/java/br/com/baleco/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/baleco/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package br.com.baleco.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.baleco.cursomc.domain.Categoria;
 import br.com.baleco.cursomc.repositories.CategoriaRepository;
+import br.com.baleco.cursomc.services.exceptions.DataIntegrityException;
 import br.com.baleco.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,5 +33,17 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que contenha produtos cadastrados");
+			
+		}
+		
 	}
 }
